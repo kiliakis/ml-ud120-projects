72ce5677446a1cbb3abbe98a324deafff98e3214
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2017-10-22 16:55:09 +0200

    preparing the final_project

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -2,48 +2,47 @@
 
 import pickle
 import numpy
-numpy.random.seed(42)
-from time import time
-
-### The words (features) and authors (labels), already largely processed.
-### These files should have been created from the previous (Lesson 10)
-### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
-
-
-
-### test_size is the percentage of events assigned to the test set (the
-### remainder go into training)
-### feature matrices changed to dense representations for compatibility with
-### classifier functions in versions 0.15.2 and earlier
 from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
-
 from sklearn.feature_extraction.text import TfidfVectorizer
+from time import time
+numpy.random.seed(42)
+
+# The words (features) and authors (labels), already largely processed.
+# These files should have been created from the previous (Lesson 10)
+# mini-project.
+words_file = "../text_learning/your_word_data.pkl"
+authors_file = "../text_learning/your_email_authors.pkl"
+word_data = pickle.load(open(words_file, "r"))
+authors = pickle.load(open(authors_file, "r"))
+
+
+# test_size is the percentage of events assigned to the test set (the
+# remainder go into training)
+# feature matrices changed to dense representations for compatibility with
+# classifier functions in versions 0.15.2 and earlier
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+    word_data, authors, test_size=0.1, random_state=42)
+
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
-features_test  = vectorizer.transform(features_test).toarray()
+features_test = vectorizer.transform(features_test).toarray()
 
 
-### a classic way to overfit is to use a small number
-### of data points and a large number of features;
-### train on only 150 events to put ourselves in this regime
+# a classic way to overfit is to use a small number
+# of data points and a large number of features;
+# train on only 150 events to put ourselves in this regime
 features_train = features_train[:150].toarray()
-labels_train   = labels_train[:150]
+labels_train = labels_train[:150]
 
 
-
-### your code goes here
+# your code goes here
 
 from sklearn.tree import DecisionTreeClassifier as dt
 clf = dt()
 t0 = time()
 clf.fit(features_train[:], labels_train[:])
-print 'Training time:', round(time()-t0, 3), 's'
+print 'Training time:', round(time() - t0, 3), 's'
 
 
 t0 = time()
@@ -51,18 +50,18 @@ pred = clf.predict(features_test).tolist()
 print 'Chis Occurrences:', pred.count(1)
 print 'Shara Occurrences:', pred.count(0)
 # print 'Predictions: ', pred
-print 'Predicting time:', round(time()-t0, 3), 's'
+print 'Predicting time:', round(time() - t0, 3), 's'
 
 
 from sklearn.metrics import accuracy_score
 
 t0 = time()
 accuracy = accuracy_score(pred, labels_test)
-print 'Accuracy score time:', round(time()-t0, 3), 's'
+print 'Accuracy score time:', round(time() - t0, 3), 's'
 
 # accuracy = clf.score(features_test, labels_test)
-print 'The accuracy is:', accuracy 
-#########################################################
+print 'The accuracy is:', accuracy
+#######################################################
 
 arr = clf.feature_importances_
 print 'Top importance: %lf' % max(arr)
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -7,49 +7,93 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+# Task 1: Select what features you'll use.
+# features_list is a list of strings, each of which is a feature name.
+# The first feature must be "poi".
+# features_list = ['poi', 'salary']  # You will need to use more features
+'''
+The num of features is not that much (21), I will start with all of them
+and then use pca to limit them to ~10
+'''
 
-### Load the dictionary containing the dataset
+all_features = ['bonus', 'deferral_payments',
+                'deferred_income', 'director_fees', 'email_address',
+                'exercised_stock_options', 'expenses', 'from_messages',
+                'from_poi_to_this_person', 'from_this_person_to_poi',
+                'loan_advances', 'long_term_incentinve', 'other',
+                'poi', 'restricted_stock', 'restricted_stock_deferred',
+                'salary', 'shared_receipt_with_poi', 'to_messages',
+                'total_payments', 'total_stock_value']
+
+features_list = all_features
+
+# Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+
+# sys.exit()
+# Task 2: Remove outliers
+'''
+I will use the outlier cleaner to clean up a bit the data_dict,
+perhaps it requires diffent action per feature
+'''
+# Task 3: Create new feature(s)
+'''
+I need to create at least 1-2 more features
+feature1 = 
+feature2 = 
+It would be interesting to check on the text of all mails and 
+use the invert frequency to add new features, perhaps there are some words
+present in the emails that denote a POI
+'''
+# Store to my_dataset for easy export below.
 my_dataset = data_dict
 
-### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+# Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
+# Task 4: Try a varity of classifiers
+'''
+Try 3-4 classifiers, adaboost, randomforest, knearest etc
++ PCA to reduce the num of features to ~10 (without the email text data)
+'''
+
+# Please name your classifier clf for easy export below.
+# Note that if you want to do PCA or other multi-stage operations,
+# you'll need to use Pipelines. For more info:
+# http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+# Task 5: Tune your classifier to achieve better than .3 precision and recall
+# using our testing script. Check the tester.py script in the final project
+# folder for details on the evaluation method, especially the test_classifier
+# function. Because of the small size of the dataset, the script uses
+# stratified shuffle split cross validation. For more info:
+# http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+'''
+Use the funtion that automatically tunes the classifiers with 2-3 attributes
+each with ~10 different values
+'''
+
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+'''
+Use different combinations of test data, train data to find the best combination
+'''
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+
+# Task 6: Dump your classifier, dataset, and features_list so anyone can
+# check your results. You do not need to change anything below, but make sure
+# that the version of poi_id.py that you submit can be run on its own and
+# generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
